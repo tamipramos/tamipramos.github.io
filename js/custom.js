b49caa892681ce57871cdb6bca3733d61b0254b4
tamipramos <tamipramos@gmail.com>
2022-06-19 21:32:58 +0100

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -6,7 +6,7 @@ Theme by: WebThemez.com
 Note: Please use our back link in your site
 */
 $( function() {
-        var endDate = "Jun  21, 2022 19:00:00";
+        var endDate = "June 21, 2022 19:00:00";
 
         $('.countdown.simple').countdown({ date: endDate });
 
